66884603e7b4b31b9f1cfb592ec85e73c9c45e8b
James Curtis <jameswcurtis94@outlook.com>
2024-08-21 10:19:47 +0000

    API key obtained and added into code along with an evet listener

--- scripts.js
+++ scripts.js
@@ -4,3 +4,11 @@ const weatherBox = document.querySelector('.weather-box');
 const weatherDetails = document.querySelector('.weather-details');
 const error404 = document.querySelector('.not-found');
 const cityHide = document.querySelector('.city-hide');
+
+searchButton.addEventListener('click', () => {
+
+    const APIKey = 'b99ccba921cc90a842cd466fe4d0e135';
+    const city = document.querySelector('.search-box input').value.trim();
+
+    if (city === '')
+        return;
